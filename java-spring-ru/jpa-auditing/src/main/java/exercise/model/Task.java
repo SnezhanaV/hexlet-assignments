4711d08207542bd0da0f507a07e877100e6abde6
@hexlet/cli <support@hexlet.io>
2024-10-01 23:19:57 +0600

    submit java-spring-ru/jpa-auditing

--- java-spring-ru/jpa-auditing/src/main/java/exercise/model/Task.java
+++ java-spring-ru/jpa-auditing/src/main/java/exercise/model/Task.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
