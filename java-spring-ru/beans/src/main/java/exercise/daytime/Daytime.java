5b31d04d9aba64bcc369e62b7d93e3049a5c8d92
@hexlet/cli <support@hexlet.io>
2024-10-03 09:59:55 +0600

    submit java-spring-ru/beans

---
+++ java-spring-ru/beans/src/main/java/exercise/daytime/Daytime.java
@@ -0,0 +1,10 @@
+package exercise.daytime;
+
+// Интерфейс содержит метод, возвращающий название времени суток
+// Реализация методов представлена в классах Morning, Day, Evening, Night,
+// которые реализуют этот интерфейс
+//@Component
+public interface Daytime {
+
+    String getName();
+}
